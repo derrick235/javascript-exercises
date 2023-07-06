1f2fa870c5bb0334c05cb0c9c208190d6e54b6dc
Derrick Lu <44071958+derrick235@users.noreply.github.com>
2023-07-05 21:31:30 -0400

    Reverse given stirng in ex03
    Reverse given string by using substring method and a loop
    until all characters have been placed in result.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let result = "";
+  while (str.length != 0) {
+    result += str.substring(str.length - 1);
+    str = str.substring(0, str.length - 1);
+  }
+  return result;
 };
 
 // Do not edit below this line
