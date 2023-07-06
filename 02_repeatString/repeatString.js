070b8dc7605f0f948cc45f7addf36767118fb577
Derrick Lu <44071958+derrick235@users.noreply.github.com>
2023-07-05 21:22:21 -0400

    Complete ex02 where strings repeat given num
    Add for loop to repeat a string a given number of times and return
    an error if the given number is negative.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, numRepeat) {
+  if (numRepeat < 0) {
+    return "ERROR";
+  }
+  let result = "";
+  for (let i = 0; i < numRepeat; i++) {
+    result += str;
+  }
+  return result;
 };
 
 // Do not edit below this line
