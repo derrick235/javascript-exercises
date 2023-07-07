13b3fcdd231e20e410daf77a6ab0eea90ce73055
Derrick Lu <44071958+derrick235@users.noreply.github.com>
2023-07-06 22:27:32 -0400

    Add code for ex04 that removes array elements
    Add code for ex04 that views a variable amount of parameters
    and removes its appearances from the array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removal) {
+  for (let i = 0; i < removal.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (removal[i] === arr[j]) {
+        arr.splice(j, 1);
+        break;
+      }
+    }
+  }
 
+  return arr;
 };
 
 // Do not edit below this line
