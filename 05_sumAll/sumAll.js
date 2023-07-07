dde1075810c9edeaf0dc5d6a6398d83158874f41
Derrick Lu <44071958+derrick235@users.noreply.github.com>
2023-07-06 22:37:48 -0400

    Find sum of all numbers in a given range
    Complete ex05 where given a range, find sum of all numbers within
    the range. Error is passed if numbers are negative or not of type
    number.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+  // error thrown if type of arguments are not number or if numbers are negative
+  if ((typeof start) !== 'number' || (typeof end) !== 'number' || start < 0 || end < 0) {
+    return 'ERROR';
+  }
+  let sum = 0;
+  // Test if starting num is greater than ending num and swap if so
+  if (start > end) {
+    let temp = end;
+    end = start;
+    start = temp;
+  }
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
